41c68ee26a3597601e84122ec0b574b0ab5facd1
Brandon Lee Schultz <schultzblee@gmail.com>
2024-12-17 22:55:12 +0200

    fix(config): Update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
+    env: {
+        POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
+    }
 };
 
 export default nextConfig;
